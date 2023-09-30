679b03b219c0881a3d440934129cf84e82fa39ee
isaac albor <alborisaac@coruniamericana.edu.co>
2023-09-30 17:36:07 -0500

    termine vistas hay que arreglar el redirecionamiento

--- src/app/alojamiento/alojamiento.page.ts
+++ src/app/alojamiento/alojamiento.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import dataInfo from '../../../files/estadia.json';
+
 
 @Component({
   selector: 'app-alojamiento',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alojamiento.page.scss'],
 })
 export class AlojamientoPage implements OnInit {
+  infoCards = dataInfo.data;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import dataInfo from '../../../files/home.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  infoCards = dataInfo.data;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+  }
 
 }
